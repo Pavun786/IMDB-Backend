ae6c2c071c95c4755b3896ab031e44fe493292db
Pavun786 <pulsarpavuns786@gmail.com>
2024-02-22 13:35:03 +0530

    orgin_Added

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ dotenv.config()
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
-app.use(cors("*"))
+app.use(cors({
+    origin : "https://imdb-frontend-ruby.vercel.app"
+}))
 
 dbConnection()
 
